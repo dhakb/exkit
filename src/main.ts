570f00996e081358c37f7b4e33983cfac2e26422
Giga Mania <hil.giga@gmail.com>
2025-05-30 11:16:07 +0100

    chore: add descriptive comments to app entry point for better readability

--- src/main.ts
+++ src/main.ts
@@ -15,27 +15,34 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 async function main() {
+  /* Disable the 'X-Powered-By' header to avoid exposing Express in HTTP responses */
   app.disable('x-powered-by');
 
-  app.set('trust-proxy', 1);
+  /* Trust the first proxy (e.g., Cloudflare) to use the correct client IP from the 'X-Forwarded-For' header -- */
+  app.set('trust proxy', 1);
 
+  /* -- Parse the body of the request */
   app.use(express.json());
 
+  /* -- Parse URL-encoded bodies (as sent by HTML forms) -- */
   app.use(express.urlencoded({ extended: true }));
 
+  /* -- Trim the incoming request body; remove any leading/trailing whitespace --  */
   app.use(requestBodyTrimMiddleware);
 
-  app.use(express.urlencoded({ extended: true }));
-
+  /* -- Initialize Swagger API documentation -- */
   setupSwagger({ app });
 
-  //  -- Routes --
+  /* -- Register API router and apply global rate limiting -- */
   app.use('/api', globalThrottler, apiRouter);
 
+  /* -- Handle request to unknown routes (404 Not Found) -- */
   app.use(unknownRoutesMiddleware);
 
+  /* -- Handle errors and send standardized error responses -- */
   app.use(exceptionsMiddleware);
 
+  /* -- Start the server -- */
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on port: ${PORT}`);
   });
